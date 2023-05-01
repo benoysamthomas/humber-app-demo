dbd043ac4bce8cc5e3fdcf50e23bc90f9a0c8964
benoy <n01613564@humber.ca>
2023-05-01 13:54:12 -0400

    commit:5- Shared Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BannerComponent
+    BannerComponent,
+    NavbarComponent,
+    FooterComponent
 
   ],
   imports: [
