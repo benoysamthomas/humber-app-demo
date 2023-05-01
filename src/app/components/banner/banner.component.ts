98895667288f748f551643962057ca368ae85177
benoy <n01613564@humber.ca>
2023-05-01 15:40:03 -0400

    commit 7: Bindings

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./banner.component.css']
 })
 export class BannerComponent {
-
+banner={
+heading:"Spring Season is backssss!",
+subheading:"And so is our offers too then why waiting",
+cta:"checkout all our products",
+url:"https://www.amazon.ca",
+}
+comments="";
+handleClick(){
+  console.log("clicked the button")
+}
 }
